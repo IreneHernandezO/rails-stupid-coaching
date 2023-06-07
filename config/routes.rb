be34886d6f9e91b5452e6f6490fa91e9ffe10cbe
irene hernandez <hernandez.o.irene@gmail.com>
2023-06-07 15:14:58 +0800

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "ask", to: "questions#ask"
+  get "answer", to: "questions#answer"
+
+  #reate a new POST route for the URL http://localhost:3000/contact that points to a
+  #new leave_feedback action in the StaticPagesController class, and name the new route
+  #'leave_feedback'. The new route should look like this:
 end
